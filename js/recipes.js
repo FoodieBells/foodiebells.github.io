503782eee14b987ebbe468e8d007b285715a8ecc
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-02-01 23:45:49 -0500

    blogoverview

--- js/recipes.js
+++ js/recipes.js
@@ -12,7 +12,7 @@ $(window).on('load', function() {
                 for (i in data){
                     recipes += '<div class="col-lg-4 col-md-6">'
                         +'<div class="recipe">'
-                            +'<img src="img/recipes/11.jpg" alt="">'
+                            +'<img src="../img/recipes/11.jpg" alt="">'
                             +'<div class="recipe-info-warp">'
                             +'<div class="recipe-info">'
                             +'<h3>' + data[i].data.name + '</h3>'
