a52010a2cbeac03cd15931f6238d384c59b76342
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-07-04 00:50:18 -0400

    deploy rectified

--- foodiebells/src/components/home/instagram-feed/Actions.js
+++
@@ -1,9 +0,0 @@
-export function fetchData( url, cb ) {
-	window.myJsonpCallback = function(data) {
-	    cb( data )
-	    scriptEl.remove()
-	};
-	var scriptEl = document.createElement('script');
-	scriptEl.setAttribute( 'src',url );
-	document.body.appendChild(scriptEl)
-};
